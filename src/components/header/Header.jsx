e3e54ab685094984ed0f974d5b45f2a2b3867554
tamimhasan19702 <tamimhasancu@gmail.com>
2022-12-13 21:03:16 +0600

    header layout updates

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,8 +1,53 @@
-import React from 'react'
+import React,{useRef} from 'react';
+import './header.scss';
+import Logo from '../../assets/logo.png';
+import { Link,useLocation } from 'react-router-dom';
+
+
+const headerNav = [
+    {
+        display: 'Home',
+        path: '/'
+    },
+    {
+        display: 'Movies',
+        path: '/movie'
+    },
+    {
+        display: 'Tv Series',
+        path: '/tv'
+    }
+];
 
 function Header() {
+
+  const { pathname } = useLocation();
+  const headerRef = useRef(null);
+
+  const active = headerNav.findIndex(e => e.path === pathname )
+ 
   return (
-    <div>Header</div>
+    <div ref={headerRef} className="header">
+        <div className="header-wrap container">
+            <div className="logo">
+                <img src={Logo} alt="Logo" />
+                <Link to="/">TmMovies</Link>
+            </div>
+            <ul className="header-nav">
+                {
+                    headerNav.map((e,i) => (
+                        <li key={i} className={`${i === active ? 'active' : ''}`}>
+                        
+                        <Link to={e.path}>
+                           {e.display}
+                        </Link>
+
+                        </li>
+                    ))
+                }
+            </ul>
+        </div>
+    </div>
   )
 }
 
