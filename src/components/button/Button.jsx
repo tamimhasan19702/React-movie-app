b6d342f330a9f781daeac2a2a648fa5143599966
tamimhasan19702 <tamimhasancu@gmail.com>
2022-12-13 23:11:08 +0600

    button component updates with layout

---
+++ src/components/button/Button.jsx
@@ -0,0 +1,31 @@
+import React from 'react';
+import './button.scss';
+import PropTypes from 'prop-types';
+
+const Button = (props) => {
+  return (
+    <button
+    className={`btn ${props.className}`}
+    onClick={props.onClick ? () => props.onClick() : null }
+    >
+      {props.children}
+    </button>
+  )
+}
+
+const OutLineButton = props => {
+    return (
+      <Button
+      className={`btn-outline ${props.className}`}
+      onClick={props.onClick ? () => props.onClick() : null }
+      >
+        {props.children}
+      </Button>
+    )    
+}
+
+Button.propTypes = {
+onClick: PropTypes.func
+}
+
+export default Button
