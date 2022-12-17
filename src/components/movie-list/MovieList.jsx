3a96458f7c11c567ac7de432fd58cbdc434c1a0c
tamimhasan19702 <tamimhasancu@gmail.com>
2022-12-17 19:22:13 +0600

    movie list hooks setup

--- src/components/movie-list/MovieList.jsx
+++ src/components/movie-list/MovieList.jsx
@@ -1,7 +1,42 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 import PropTypes from 'prop-types';
+import './movielist.scss';
+import { SwiperSlide,Swiper } from 'swiper/swiper-react';
+import { Link } from 'react-router-dom';
+import Button from '../button/Button';
 
-const MovieList = () => {
+import tmdbApi, { category } from '../../api/tmdbApi';
+import apiConfig from '../../api/apiConfig';
+
+const MovieList = (props) => {
+
+  const [item, setItems] = useState([]);
+
+  useEffect(() => {
+   const getList = async () => {
+    let response = null;
+    const params = {};
+
+    if(props.type !== 'similar'){
+
+    switch(props.category){
+        case category.movie: 
+        response =  await tmdbApi.getMoviesList(props.type, {params});
+        break;
+        default: 
+        response = await tmdbApi.getTvList(props.type, {params})
+      }
+
+    }
+    else{
+      response = await tmdbApi.similar(props.category, props.id);
+    }
+    setItems(response.results);
+   }
+   getList();
+  }, []);
+
+
   return (
     <div>
       
@@ -10,7 +45,8 @@ const MovieList = () => {
 }
 
 MovieList.propTypes = {
-    
+    category: PropTypes.string.isRequired,
+    type: PropTypes.string.isRequired
 }
 
 export default MovieList;
