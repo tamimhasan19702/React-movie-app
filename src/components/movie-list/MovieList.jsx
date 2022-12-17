d2efa48a7af287299637d9556027cf86ff07e833
tamimhasan19702 <tamimhasancu@gmail.com>
2022-12-17 19:06:31 +0600

    home page making done

---
+++ src/components/movie-list/MovieList.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+const MovieList = () => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+MovieList.propTypes = {
+    
+}
+
+export default MovieList;
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,13 +1,29 @@
 import React from 'react';
 import HeroSlide from '../components/hero-slide/Hero-slide';
+import { Link } from 'react-router-dom';
+import { OutLineButton } from '../components/button/Button';
 
 
 const Home = () => {
   return (
-    <div>
+    <>
       <HeroSlide/>
-    </div>
-  )
+
+      <div className="container">
+        <div className="section mb-3">
+          <div className="section-header mb-2">
+            <h2>Trending Movies</h2>
+            <Link to='/movie'>
+             <OutLineButton className="small">
+              View more
+              </OutLineButton>
+            </Link>
+          </div>
+        </div>
+      </div>
+
+    </>
+  );
 }
 
 export default Home
