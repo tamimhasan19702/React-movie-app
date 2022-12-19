cbb7de7b7bd7db9d05dbf32ebb0884ecd38a65d2
tamimhasan19702 <tamimhasancu@gmail.com>
2022-12-19 19:59:51 +0600

    updated movie card list items

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,4 +1,9 @@
-import React from 'react'
+import React from 'react';
+import './footer.scss';
+import { Link } from 'react-router-dom';
+import bg from '../../assets/footer-bg.png';
+import Logo from '../../assets/logo-update.png';
+
 
 function Footer() {
   return (
