e8dcd35c1e4569e403cf9ec0110eaf12a28c7a33
tamimhasan19702 <tamimhasancu@gmail.com>
2022-12-19 20:05:14 +0600

    footer layout updates

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,14 +1,30 @@
 import React from 'react';
 import './footer.scss';
 import { Link } from 'react-router-dom';
-import bg from '../../assets/footer-bg.png';
+import bg from '../../assets/footer-bg.jpg'
 import Logo from '../../assets/logo-update.png';
 
 
 function Footer() {
   return (
-    <div>
-      Footer
+    <div className='footer' style={{backgroundImage: `url(${bg})`}}>
+     <div className="footer-content container">
+      <div className="logo">
+        <img src={Logo} alt="logo" />
+        <Link to='/'>Movies</Link>
+      </div>
+     </div>
+     <div className="footer-content-menus">
+      <div className="footer-content-menu">
+        <Link to='/'>Home</Link>
+        <Link to='/'>Contact Us</Link>
+        <Link to='/'>Terms of Service</Link>
+        <Link to='/'>About Us</Link>
+      </div>
+      <div className="footer-content-menu">
+        
+      </div>
+     </div>
     </div>
   )
 }
