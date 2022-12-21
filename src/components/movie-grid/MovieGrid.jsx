f0cd7e8c4181050b8b82cf50da5e929e93f48119
tamimhasan19702 <tamimhasancu@gmail.com>
2022-12-21 13:33:24 +0600

    use params error fix and moviegrid base is working

--- src/components/movie-grid/MovieGrid.jsx
+++ src/components/movie-grid/MovieGrid.jsx
@@ -1,14 +1,16 @@
 import React, { useEffect, useState } from 'react';
 import './moviegrid.scss';
 import MovieCard from '../movieCard/MovieCard';
-import { useParams } from 'react-router';
+import { useParams } from 'react-router-dom';
 import tmdbApi, { category, movieType, tvType } from '../../api/tmdbApi';
 
 const MovieGrid = (props) => {
 
    const [items, setItems] = useState([]);
+
    const [page, setPage] = useState(1);
    const [totalPage, setTotalPage] = useState(0);
+
    const {keyword} = useParams();
 
    useEffect(() => {
@@ -17,22 +19,34 @@ const MovieGrid = (props) => {
 
         if( keyword === undefined){
          const params = {};
+
          switch(props.category){
+
             case category.movie :
              response = await tmdbApi.getMoviesList(movieType.upcoming, {params});
              break;
+
             default : 
              response =  await tmdbApi.getTvList(tvType.popular, {params}); 
+
            }
         }else{
-      
+         const params = {
+            query: keyword
+         }
+         response = await tmdbApi.search(props.category, {params});
         }
+        setItems(response.results)
+        setTotalPage(response.total_pages)
     }
-   }, []);
+    getList();
+   }, [props.category, keyword]);
 
   return (
-    <div>
-      Movie grid
+    <div className='movie-grid'>
+      {
+        items.map((item, i) => <MovieCard category={props.category} item={item} key={i}/>)
+      }
     </div>
   )
 }
