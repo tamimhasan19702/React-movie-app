31f425bfebffc3a87eee5408ab4296997155fe19
tamimhasan19702 <tamimhasancu@gmail.com>
2022-12-22 16:46:51 +0600

    details styles updates

--- src/pages/details/Detail.jsx
+++ src/pages/details/Detail.jsx
@@ -1,8 +1,8 @@
 import React, { useEffect, useState } from 'react';
-import { useParams } from 'react-router';
+import { useParams } from 'react-router-dom';
 import tmdbApi from '../../api/tmdbApi';
 import apiConfig from '../../api/apiConfig';
-
+import './detail.scss';
 
 const Detail = () => {
 
@@ -23,8 +23,22 @@ const Detail = () => {
     <>
       {
         item && (
+          <>  
           <div className='banner' style={{backgroundImage: `url(${apiConfig.originalImage(item.backdrop_path || item.poster_path)})`}}>
           </div>
+
+          <div className="mb-3 movie-content container">
+            <div className="movie-content-poster">
+              <div className="movie-content-poster-img" style={{backgroundImage: `url(${apiConfig.originalImage(item.poster_path || item.backdrop_path)})`}}>
+
+                </div>
+             </div>  
+             <div className="movie-content-info">
+              
+             </div>
+          </div>          
+          
+          </>
         )
       }
     </>
