d067f8547b58f2beb1daa0ad1f067975be7297cf
tamimhasan19702 <tamimhasancu@gmail.com>
2022-12-25 23:09:24 +0600

    sign component added and styled and added all the condition

--- src/components/Sign-in/Sign-in.jsx
+++ src/components/Sign-in/Sign-in.jsx
@@ -1,12 +1,69 @@
 import React, { Component } from 'react';
-import './sign-in.scss'
+import './sign-in.scss';
+import FormInput from '../FormInput/FormInput';
+import Button, { OutLineButton } from '../button/Button';
 
 class SignIn extends Component {
+
+  constructor(props){
+    super(props)
+
+    this.state = {
+      email: '',
+      password: '',
+    }
+  }
+
+  // handleSubmit = async e => {
+  //   e.preventDefault();
+
+  //   const {email}
+  // }
+
+ 
+  handleChange = e => {
+
+    const { value,name } = e.target;
+
+    this.setState({ [name] : value })
+  }
+
+
   render() {
     return (
       <div className='sign-in'>
          <h2>I already have an account</h2>
          <span>Sign in with your email and password</span>
+
+         <form>
+
+         <FormInput 
+         name="email"
+         value={this.state.email}
+         required
+         handleChange={this.handleChange}
+         type='Email'
+         label='Email'/>        
+         
+         <FormInput 
+         name="password"
+         value={this.state.password}
+         required
+         handleChange={this.handleChange}
+         type='password'
+         label='Password'/>
+
+         <div className="buttons">
+          
+         <Button type='submit'> Sign In</Button>
+
+         <OutLineButton 
+         onClick = {signInWithGoogle}
+         >Sign In With Google</OutLineButton>
+
+         </div>        
+
+         </form>
       </div>
     )
   }
