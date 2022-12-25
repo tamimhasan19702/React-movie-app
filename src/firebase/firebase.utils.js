7f8b7ec7a436bcf9b0fe5420bd23d887a2bdb88f
tamimhasan19702 <tamimhasancu@gmail.com>
2022-12-25 23:48:00 +0600

    firebase config added

---
+++ src/firebase/firebase.utils.js
@@ -0,0 +1,13 @@
+import firebase from 'firebase/compat/app';
+import 'firebase/compat/auth';
+import 'firebase/compat/firestore';
+
+const config = {
+    apiKey: "AIzaSyD6Uj-uO11mw2zgj0FV0O_GdvZIg7UHsFg",
+    authDomain: "tmmovie-website.firebaseapp.com",
+    projectId: "tmmovie-website",
+    storageBucket: "tmmovie-website.appspot.com",
+    messagingSenderId: "397072549205",
+    appId: "1:397072549205:web:2d24d58fd00df2d45b1bdc",
+    measurementId: "G-371PWHD4JP"
+}
