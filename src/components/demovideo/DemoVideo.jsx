61efb8f4a267fb34d241d4e9eb000bf1bfa0a9c9
tamimhasan19702 <tamimhasancu@gmail.com>
2023-01-01 21:50:21 +0600

    fixed few bugs

--- src/components/demovideo/DemoVideo.jsx
+++ src/components/demovideo/DemoVideo.jsx
@@ -11,7 +11,7 @@ return(
 {
     currentUser ? (
       <div className='movie-video'>
-                <iframe width="120%" height="1000px" src="https://www.youtube.com/embed/Uy6qr_l5vAE?autoplay=1" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen="allowfullscreen"
+                <iframe src="https://www.youtube.com/embed/Uy6qr_l5vAE?autoplay=1" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen="allowfullscreen"
                 mozallowfullscreen="mozallowfullscreen" 
                 msallowfullscreen="msallowfullscreen" 
                 oallowfullscreen="oallowfullscreen" 
