72ae7b9d31f7491acf49a04332eb5765cbb748f1
tamimhasan19702 <tamimhasancu@gmail.com>
2023-01-09 01:45:28 +0600

    details updates

--- src/pages/details/Detail.jsx
+++ src/pages/details/Detail.jsx
@@ -103,7 +103,7 @@ const Detail = ({currentUser}) => {
 
             ) : (
               <div className='login-alert'>
-             <h1>Please Sign in to view the movie</h1>
+             <h1>Please Sign in to view the contents</h1>
              <Button className='button'>
                                  <a 
                                  href='https://tmmovie-website.web.app/sign-in'
